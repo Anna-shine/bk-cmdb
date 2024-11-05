bc403551bb61f3109918bf58b5f98f626d28b25e
Anna-shine <2496253516@qq.com>
2024-11-05 10:05:54 +0800

    feat: change supplyaccount to tenantID

--- src/common/metadata/inst.go
+++ src/common/metadata/inst.go
@@ -44,7 +44,7 @@ type ModuleInst struct {
 	SetID             int64  `bson:"bk_set_id" json:"bk_set_id" field:"bk_set_id" mapstructure:"bk_set_id"`
 	ModuleID          int64  `bson:"bk_module_id" json:"bk_module_id" field:"bk_module_id" mapstructure:"bk_module_id"`
 	ModuleName        string `bson:"bk_module_name" json:"bk_module_name" field:"bk_module_name" mapstructure:"bk_module_name"`
-	SupplierAccount   string `bson:"bk_supplier_account" json:"bk_supplier_account" field:"bk_supplier_account" mapstructure:"bk_supplier_account"`
+	TenantID          string `bson:"tenant_id" json:"tenant_id" field:"tenant_id" mapstructure:"tenant_id"`
 	ServiceCategoryID int64  `bson:"service_category_id" json:"service_category_id" field:"service_category_id" mapstructure:"service_category_id"`
 	ServiceTemplateID int64  `bson:"service_template_id" json:"service_template_id" field:"service_template_id" mapstructure:"service_template_id"`
 	ParentID          int64  `bson:"bk_parent_id" json:"bk_parent_id" field:"bk_parent_id" mapstructure:"bk_parent_id"`
